9f819f55116ac2286d5da1faa72b3e46ea90b84e
nolte <nolte07@googlemail.com>
2020-08-01 15:06:59 +0200

    simple self sign certs

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ``` bash
 bash <(curl -s https://raw.githubusercontent.com/nolte/terraform-provider-harbor/develop/scripts/tst-00-prepare-kind.sh)
 ```
+
+``` bash
+cd ./examples/test/
+go test --tags=integration -timeout 20m -v
+```
--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -4,27 +4,24 @@ resource "kubernetes_namespace" "operators" {
   }
 }
 
-data "http" "template" {
-  url = "https://raw.githubusercontent.com/coreos/prometheus-operator/release-0.38/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml"
+module "prometheus_crds" {
+  source                     = "git::https://github.com/nolte/tf-k8s-monitoring.git//modules/crds"
+  enable_crd_servicemonitors = true
+  enable_crd_prometheusrules = false
+  enable_crd_alertmanagers   = false
+  enable_crd_podmonitors     = false
+  enable_crd_prometheuses    = false
+  enable_crd_thanosrulers    = false
 }
-
-#data "template_file" "init" {
-#  template =
-#}
-#
-resource "kubectl_manifest" "test" {
-  yaml_body = data.http.template.body
-}
-
 
 module "certmanager" {
-  depends_list      = [kubectl_manifest.test]
+  depends_list      = [module.prometheus_crds.depend_on]
   source            = "../../modules/install"
   release_namespace = kubernetes_namespace.operators.metadata[0].name
 }
 
 # give Certsmanger Time to Work
-resource "time_sleep" "wait_30_seconds" {
+resource "time_sleep" "wait_seconds" {
   depends_on = [module.certmanager.depend_on]
 
   create_duration = "15s"
@@ -32,7 +29,7 @@ resource "time_sleep" "wait_30_seconds" {
 
 module "selfsigned_ca" {
   depends_list = [
-    time_sleep.wait_30_seconds,
+    time_sleep.wait_seconds,
     module.certmanager.depend_on
   ]
   source = "../../modules/selfsigned_ca"
